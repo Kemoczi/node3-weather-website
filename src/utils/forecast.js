626f9397677e91d4e11f3bf83030623fa8a54e91
Krajewski <tkraj@dolby.com>
2021-11-12 22:43:20 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const forecast = (lat, lon, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=db5227c12ee0a558491f014c086747cd&query=' + lat + ',' + lon +'&units=m'
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Error! Unable to find location.', undefined)
+        } else {
+            callback(undefined,
+                body.current.weather_descriptions[0] + '.'
+                + " It's currently " + body.current.temperature
+                + " degrees out. It feels like "
+                + body.current.feelslike + " degrees out."
+            )
+        }
+    })
+}
+
+module.exports = forecast
